3638f1de14f542ffc5e8c0384c45605a767e94b8
PlantaH <laplantah@gmail.com>
2021-10-13 16:08:50 -0300

    primera entrega proyecto final

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -10,52 +10,58 @@ import ItemList from "../ItemList/ItemList";
 
 const ItemListContainer = ({nombre,totalItemsCarrito,setTotalItemsCarrito}) => {
     
+  
     const {name} = useParams() //para tomar el parametro del link
     
     const [productos, setProductos] = useState([]);
     const [loading, setLoading] = useState(true)
 
-    const getProductos = new Promise((resolve,reject) => {
-        setTimeout( ()=> {
-        resolve(listaProductos)
-        },1000)
-    })
+    
 
-    const getProductosJSON = async(categoria) =>{
-        try{
-            const result = await getProductos;
-
-            let result_filtro = result.filter(function(p) { return p.categoria === categoria; });
-
-            setProductos(result_filtro)        
-
-            setLoading(false)
-
-        } catch (error){
-            console.log(error)
-        }
-    } 
-
-    const getDestacadosJSON = async() =>{
-        try{
-            const result = await getProductos;
-            
-            let result_filtro = result.filter(function(p) {
-                return p.home === "S"; });
-
-            setProductos(result_filtro)
-
-            setLoading(false)
-        } catch (error){
-            console.log(error)
-        }
-    } 
-
-     
+    
+    
     useEffect(() => {   
-        setLoading(true);       
+        setLoading(true);    
+        
+        const getProductos = new Promise((resolve,reject) => {
+            setTimeout( ()=> {
+            resolve(listaProductos)
+            },1000)
+        })
+
+        const getProductosJSON = async(categoria) =>{
+            try{
+                const result = await getProductos;
+    
+                let result_filtro = result.filter(function(p) { return p.categoria === categoria; });
+    
+                setProductos(result_filtro)        
+    
+                setLoading(false)
+    
+            } catch (error){
+                console.log(error)
+            }
+        } 
+    
+        const getDestacadosJSON = async() =>{
+            try{
+                const result = await getProductos;
+                
+                let result_filtro = result.filter(function(p) { return p.home === "S"; });
+    
+                setProductos(result_filtro)
+    
+                setLoading(false)
+            } catch (error){
+                console.log(error)
+            }
+        } 
+    
+        
         name ? getProductosJSON(name) :  getDestacadosJSON()        
-      }, [name]);
+
+    } , [name]);
 
 
     return (        
@@ -65,7 +71,7 @@ const ItemListContainer = ({nombre,totalItemsCarrito,setTotalItemsCarrito}) => {
                 ?
                     <Loader type="Audio" color="red" height={100} width={100} timeout={1000} />
                 :                    
-                    <ItemList items={productos} nombre={name ? name : "destacados"} items={productos} totalItemsCarrito={totalItemsCarrito} setTotalItemsCarrito={setTotalItemsCarrito} />    
+                    <ItemList items={productos} nombre={name ? name : "destacados"} totalItemsCarrito={totalItemsCarrito} setTotalItemsCarrito={setTotalItemsCarrito} />    
             }     
           
         </>       
